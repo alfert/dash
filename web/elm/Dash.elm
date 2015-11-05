9f8bef4886ec6ff541f1a3014dec5823b307f136
Klaus Alfert <klaus.alfert@gmail.com>
2015-11-05 21:01:14 +0100

    a first simple web app with Elm

--- web/elm/Dash.elm
+++ web/elm/Dash.elm
@@ -1,6 +1,33 @@
 module Dash where
 
-import Html
+import StartApp.Simple exposing (start)
 
-main =
-    Html.text "Hello from Elm"
+import Html exposing (ul, li, text, button, div)
+import Html.Attributes exposing (class)
+import Html.Events exposing (onClick)
+
+main = start { model = 0, update = update, view = view }
+
+-- MODEL
+type alias Model = Int
+
+-- UPDATE
+
+type Action = Reset | Inc | NewValue
+
+update : Action -> Model -> Model
+update action model = 
+    case action of
+        Reset -> 0
+        Inc -> model + 1
+
+-- VIEW
+view : Signal.Address Action -> Model -> Html.Html
+view address model =
+ div []
+    [ button [ onClick address Reset ] [ text "Reset" ]
+    , div [ countStyle ] [ text (toString model) ]
+    , button [ onClick address Inc ] [ text "+" ]
+    ]
+countStyle : Html.Attribute 
+countStyle = class "form.button"
