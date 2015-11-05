970f7b21109ae15af9c8aeb61423ceed6299fbc6
Klaus Alfert <klaus.alfert@gmail.com>
2015-11-05 21:19:26 +0100

    Generated Channel for a counter

---
+++ test/channels/counter_channel_test.exs
@@ -0,0 +1,28 @@
+defmodule Dash.CounterChannelTest do
+  use Dash.ChannelCase
+
+  alias Dash.CounterChannel
+
+  setup do
+    {:ok, _, socket} =
+      socket("user_id", %{some: :assign})
+      |> subscribe_and_join(CounterChannel, "counters:lobby")
+
+    {:ok, socket: socket}
+  end
+
+  test "ping replies with status ok", %{socket: socket} do
+    ref = push socket, "ping", %{"hello" => "there"}
+    assert_reply ref, :ok, %{"hello" => "there"}
+  end
+
+  test "shout broadcasts to counters:lobby", %{socket: socket} do
+    push socket, "shout", %{"hello" => "all"}
+    assert_broadcast "shout", %{"hello" => "all"}
+  end
+
+  test "broadcasts are pushed to the client", %{socket: socket} do
+    broadcast_from! socket, "broadcast", %{"some" => "data"}
+    assert_push "broadcast", %{"some" => "data"}
+  end
+end
