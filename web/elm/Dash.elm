f6f1757ff3df9600afd1ff78bf8351d0df386673
Klaus Alfert <klaus.alfert@gmail.com>
2015-11-13 23:43:26 +0100

    a complexes model with the history

--- web/elm/Dash.elm
+++ web/elm/Dash.elm
@@ -5,48 +5,65 @@ import Effects exposing (Effects, Never)
 import Task exposing (Task)
 
 import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, id)
 import Html.Events exposing (onClick)
 
 
 init : (Model, Effects Action)
 init =
-  (0, Effects.none)
+  (reset_model, Effects.none)
 
 
 -- MODEL
-type alias Model = Int
+type alias Model = (CounterType, History)
+type alias CounterType = Int
+type alias History = List Int
 
 -- UPDATE
 
-type Action = NoOp | Reset | Inc | NewValue Model
+type Action = NoOp | Reset | Inc | NewValue Int
 
 update : Action -> Model -> (Model, Effects Action)
 update action model = 
     case action of
         NoOp -> (model, Effects.none) -- do nothing
-        Reset -> (0, send_value_to_server 0) -- send 0 to the channel (= Effect)
-        Inc -> (model + 1, send_value_to_server (model + 1)) -- send the new model value
-        NewValue value -> (value, Effects.none) -- receive a new value and store it as model value
+        Reset -> (reset_model, publish_model reset_model) -- send 0 to the channel (= Effect)
+        Inc -> let m = inc_model model 
+          in (m, publish_model m) -- send the new model value
+        NewValue value -> let m = set_model value model 
+          in (m, Effects.none) -- receive a new value and store it as model value
+
+reset_model : Model
+reset_model = (0, [0])
+
+inc_model : Model -> Model
+inc_model (x, xs) = (x+1, (x+1) :: xs)
+
+set_model : Int -> Model -> Model
+set_model value (_, xs) = (value, value :: xs)
+
 
 -- EFFECTS
 
-send_value_to_server : Model -> Effects Action
-send_value_to_server model =
-  Signal.send sendValueMailBox.address model
+publish_model : Model -> Effects Action
+publish_model (x, history) =
+  Signal.send sendValueMailBox.address x
     |> Effects.task
     |> Effects.map (always NoOp)
 
 -- PORTS
 
 -- Write something towards phoenix
-port sendValuePort : Signal Model
+port sendValuePort : Signal Int
 port sendValuePort = 
     sendValueMailBox.signal 
 
 -- Get something from phoenix
-port getCounterValue : Signal Model
+port getCounterValue : Signal Int
 -- port getCounterValue = Signal.constant 0
+
+-- Send the current history to D3 time series
+-- port 
 
 -- SIGNALS
 setCounterAction: Signal Action
@@ -55,7 +72,7 @@ setCounterAction = Signal.map NewValue getCounterValue
 incomingActions : Signal Action
 incomingActions = setCounterAction
 
-sendValueMailBox : Signal.Mailbox Model
+sendValueMailBox : Signal.Mailbox Int
 sendValueMailBox =
   Signal.mailbox (0) -- initial value!?
 
@@ -66,6 +83,7 @@ view address model =
     [ button [ onClick address Reset ] [ text "Reset" ]
     , div [ countStyle ] [ text (toString model) ]
     , button [ onClick address Inc ] [ text "+" ]
+    , p [id "counterChart"] []
     ]
 countStyle : Html.Attribute 
 countStyle = class "form.button"
