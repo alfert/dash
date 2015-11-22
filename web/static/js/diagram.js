9b789603ac35dc971b9c91c9d966bfabd8ff2e61
Klaus Alfert <klaus.alfert@gmail.com>
2015-11-22 17:40:05 -0500

    Replace bostocks implementation with using the metricgraphcis library.

--- web/static/js/diagram.js
+++ web/static/js/diagram.js
@@ -1,109 +1,32 @@
-// a javascript module for a diagram made with d3.js
-//
-// this implementation is shamelessly stolen from 
-// http://bost.ocks.org/mike/chart/time-series-chart.js
-function timeSeriesChart() {
-  var margin = {top: 20, right: 20, bottom: 20, left: 20},
-      width = 760,
-      height = 120,
-      xValue = function(d) { return d[0]; },
-      yValue = function(d) { return d[1]; },
-      xScale = d3.time.scale(),
-      yScale = d3.scale.linear(),
-      xAxis = d3.svg.axis().scale(xScale).orient("bottom").tickSize(6, 0),
-      area = d3.svg.area().x(X).y1(Y),
-      line = d3.svg.line().x(X).y(Y);
+// Time series diagram using the metricgraphics 2.7
 
-  function chart(selection) {
-    selection.each(function(data) {
-
-      // Convert data to standard representation greedily;
-      // this is needed for nondeterministic accessors.
-      data = data.map(function(d, i) {
-        return [xValue.call(data, d, i), yValue.call(data, d, i)];
-      });
-
-      // Update the x-scale.
-      xScale
-          .domain(d3.extent(data, function(d) { return d[0]; }))
-          .range([0, width - margin.left - margin.right]);
-
-      // Update the y-scale.
-      yScale
-          .domain([0, d3.max(data, function(d) { return d[1]; })])
-          .range([height - margin.top - margin.bottom, 0]);
-
-      // Select the svg element, if it exists.
-      var svg = d3.select(this).selectAll("svg").data([data]);
-
-      // Otherwise, create the skeletal chart.
-      var gEnter = svg.enter().append("svg").append("g");
-      gEnter.append("path").attr("class", "area");
-      gEnter.append("path").attr("class", "line");
-      gEnter.append("g").attr("class", "x axis");
-
-      // Update the outer dimensions.
-      svg .attr("width", width)
-          .attr("height", height);
-
-      // Update the inner dimensions.
-      var g = svg.select("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-      // Update the area path.
-      g.select(".area")
-          .attr("d", area.y0(yScale.range()[0]));
-
-      // Update the line path.
-      g.select(".line")
-          .attr("d", line);
-
-      // Update the x-axis.
-      g.select(".x.axis")
-          .attr("transform", "translate(0," + yScale.range()[0] + ")")
-          .call(xAxis);
-    });
+function data_generator(length)  {
+  var d = new Date();
+  var v = 100000;
+  var data = [];
+  var seconds = 5;
+  for (var i = 0; i < length; i++) {
+    v += (Math.random() - 0.5) * 10000;
+        data.push({date: MG.clone(d), value: v});
+        d = new Date(d.getTime() + seconds * 1000);
   }
+  return data;
+};
 
-  // The x-accessor for the path generator; xScale ∘ xValue.
-  function X(d) {
-    return xScale(d[0]);
-  }
+function sample_graph(){
+  var timedData = data_generator(10);
 
-  // The x-accessor for the path generator; yScale ∘ yValue.
-  function Y(d) {
-    return yScale(d[1]);
-  }
+  // var svg = d3.select(counterDiv); 
+  console.log("timedData is" + timedData)
+  return MG.data_graphic({
+      title: "Tick Counter",
+      show_tooltips: false,
+      data: timedData,
+      target: '#counterChart',
+      width: 600,
+      height: 200,
+      right: 40
+  });
+};
 
-  chart.margin = function(_) {
-    if (!arguments.length) return margin;
-    margin = _;
-    return chart;
-  };
-
-  chart.width = function(_) {
-    if (!arguments.length) return width;
-    width = _;
-    return chart;
-  };
-
-  chart.height = function(_) {
-    if (!arguments.length) return height;
-    height = _;
-    return chart;
-  };
-
-  chart.x = function(_) {
-    if (!arguments.length) return xValue;
-    xValue = _;
-    return chart;
-  };
-
-  chart.y = function(_) {
-    if (!arguments.length) return yValue;
-    yValue = _;
-    return chart;
-  };
-
-  return chart;
-}
+export default sample_graph
