937777ff32c4c38db83547d7b8797424b768eaa2
Klaus Alfert <klaus.alfert@gmail.com>
2015-11-26 03:10:22 +0100

    log the milliseconds since epoch for each new value of the counter

--- lib/dash/counter.ex
+++ lib/dash/counter.ex
@@ -3,6 +3,11 @@ defmodule Dash.Counter do
 	Implements a simple counter as an example for state
 	"""
 
+	@type millis :: non_neg_integer
+	defstruct date: 0, value: 0 
+	@typedoc "The struct of a counter"
+	@type t :: %__MODULE__{date: millis, value: integer}
+
 	@doc """
 	Start the counter server, creates the counter `first` and starts 
 	updates every seconds
@@ -22,7 +27,7 @@ defmodule Dash.Counter do
 
 	@doc "Reset or creates a new counter"
 	def reset(counter) do
-		Agent.update(__MODULE__, fn map -> map |> Dict.put(counter, 0) end)
+		Agent.update(__MODULE__, fn map -> map |> Dict.put(counter, set_value(0)) end)
 		0
 	end
 
@@ -48,11 +53,19 @@ defmodule Dash.Counter do
 	end
 
 	@doc "Used internally to update a counter"
-	@spec update_by_inc(Dict.t, term, integer) :: {integer, Dict.t}
+	@spec update_by_inc(Dict.t, term, integer) :: {t, Dict.t}
 	def update_by_inc(map, counter, value) do
-		v = Dict.get(map, counter, 0)
-		new_map = map |> Dict.update(counter, value, fn v -> v + value end)
-		{v + value, new_map}
+		v = Dict.get(map, counter, set_value(0))
+		new_map = map |> Dict.update(counter, value, 
+			fn %__MODULE__{value: v} -> set_value(v + value) end)
+		{new_map |> Dict.fetch!(counter), new_map}
+	end
+
+	@doc "Sets the value and the current time"
+	@spec set_value(integer) :: t
+	def set_value(v) do
+		now = Timex.Time.now(:msecs)
+		%__MODULE__{value: 0, date: now}
 	end
 
 	def arb(enum) do
