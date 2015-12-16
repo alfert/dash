75a1648983221efede109d12f48086ae04db1d8e
Klaus Alfert <klaus.alfert@gmail.com>
2015-12-16 18:35:49 +0100

    Find the minimal value and add a Maybe type. Ports are now in the Dash.elm file.

--- web/elm/Dash/Diagram.elm
+++ web/elm/Dash/Diagram.elm
@@ -1,6 +1,7 @@
 module Dash.Diagram where
 
 import Time exposing (Time)
+import List exposing (..)
 
 ----------------------------------------------------------------
 -- TYPES
@@ -20,6 +21,7 @@ type alias Simple_Options = {
     , width : Int
     , height : Int
     , right : Int
+    , min_x : Maybe Float
 }
 
 -- simple_histogram
@@ -31,19 +33,25 @@ simple_histogram hist_data the_target =
     { 
         data = hist_data, 
         target = the_target, 
-        title = "Simple Elm Historgram",
+        title = "Simple Elm Histogram",
         width = 600,
         height = 200,
-        right = 40
+        right = 40,
+        min_x = min_gt_zero hist_data
     }
+
+min_gt_zero: History -> Maybe Float
+min_gt_zero hist = 
+    hist 
+        |> map (\x -> x.date) 
+        |> filter (\x -> x > 0)
+        |> minimum
+
 
 -- diagram_stream 
 diagram_stream_mailbox : Signal.Mailbox Simple_Options
 diagram_stream_mailbox = 
     Signal.mailbox({data = [], title ="", target = "#",
+        min_x = Nothing,
         width = 0, height = 0, right = 0})     -- with initial value 
-
--- Output Ports => results in drawing graph of diagram_stream via JS 
-port data_graph_port : Signal Simple_Options
-port data_graph_port = diagram_stream_mailbox.signal
 
