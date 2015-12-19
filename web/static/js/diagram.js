8e9c408829ca24726a146ca8659a94f09a4f6178
Klaus Alfert <klaus.alfert@gmail.com>
2015-12-19 11:51:52 +0100

    Conversion of integers to dates, which cannot be done properly in Elm.

--- web/static/js/diagram.js
+++ web/static/js/diagram.js
@@ -2,10 +2,28 @@
 
 // install the callback to the data_graphic function via an Elm port
 function set_diagram_port(port) {
+  console.log("subscribe to the diagram_port: " + port);
   port.subscribe(graph_options => {
+    console.log("new graph options for rendering: " + graph_options);
+    //graph_options.utc_time =true;
+    var d = convert_dates(graph_options.data); // MG.convert.date(graph_options.data, 'date', '%Y-%m-%dT%X.%L%Z');
+    graph_options.data = d;
+    console.log("converted data");
+    console.log(graph_options.data);
     return MG.data_graphic(graph_options);
   });
 };
+
+function convert_dates(data) {
+  var ds = [];
+  console.log("Convert Dates for");
+  console.log(data);
+  data.forEach(function(d, index, array) {
+    var new_date = new Date(d.date);
+    ds.push({date: new_date, value: d.value});
+  });
+  return ds;
+}
 
 function data_generator(length)  {
   var d = new Date();
@@ -14,8 +32,8 @@ function data_generator(length)  {
   var seconds = 5;
   for (var i = 0; i < length; i++) {
     v += (Math.random() - 0.5) * 10000;
-        data.push({date: MG.clone(d), value: v});
-        d = new Date(d.getTime() + seconds * 1000);
+    data.push({date: MG.clone(d), value: v});
+    d = new Date(d.getTime() + seconds * 1000);
   }
   return data;
 };
