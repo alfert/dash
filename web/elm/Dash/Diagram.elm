c7a924493953075f6467191dbfb8967de3ee1024
Klaus Alfert <klaus.alfert@gmail.com>
2015-12-21 18:18:23 +0100

    Moved a lot of Dash to Diagram to get a reusable widget

--- web/elm/Dash/Diagram.elm
+++ web/elm/Dash/Diagram.elm
@@ -2,6 +2,9 @@ module Dash.Diagram where
 
 import Time exposing (Time)
 import List exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (class, id)
+import Effects exposing (Effects, Never)
 
 ----------------------------------------------------------------
 -- TYPES
@@ -10,6 +13,9 @@ import List exposing (..)
 type alias Target = String
 type alias DataPoint = {date: Time, value: Int}
 type alias History = List DataPoint
+
+type alias Model = History
+type Action = NoOp | NewValue DataPoint
 
 -- options for simple graphs
 type alias Simple_Options = {
@@ -21,6 +27,33 @@ type alias Simple_Options = {
     , right : Int
     , min_x : Maybe Float
 }
+
+----------------------------------------------------------------
+-- Update the model
+----------------------------------------------------------------
+update : Action -> Model -> (Model, Effects Action)
+update action model = 
+    case action of
+        NoOp -> (model, Effects.none) -- do nothing
+        NewValue value -> let m = set_model value model 
+          in (m, show_diagram m) -- receive a new value and store it as model value
+
+
+-- puts a new datapoint on top of the list.
+set_model : DataPoint -> Model -> Model
+set_model value xs = Debug.log "set_model: " value :: xs
+
+-- The initial state of the model is the empty list.
+init_model : Model
+init_model = []
+
+
+----------------------------------------------------------------
+-- View the histogram
+----------------------------------------------------------------
+view_histogram : Target -> Signal.Address Action -> Model -> Html.Html 
+view_histogram diagram_id address model = 
+    p [id diagram_id] []
 
 -- simple_histogram
 -- call this function with the Model history and
@@ -45,6 +78,20 @@ min_gt_zero hist =
         |> map (\x -> x.date) 
         |> filter (\x -> x > 0)
         |> minimum
+
+----------------------------------------------------------------
+-- Update the histogram chart with new data 
+----------------------------------------------------------------
+-- send the model to draw a diagram
+show_diagram : Model -> Effects Action
+show_diagram history =
+  let 
+    eff s = s |> Effects.task |> Effects.map (always NoOp)
+    diagram = Debug.log "show_diagram: " (simple_histogram history "#elmChart")
+  in
+    Effects.batch [
+      Signal.send diagram_stream_mailbox.address diagram |> eff 
+    ]
 
 
 -- diagram_stream 
